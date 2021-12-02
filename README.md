25b3ed7d6e70a0d866e226890ea8fe7f3691efad
Austin Engle <engleau@gmail.com>
2021-12-01 23:55:51 -0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Calendar
+
+## Table Of Content
+
+- [Description](#Description)
+- [Visuals](#Visuals)
+- [Usage](#Usage)
+- [Links](#Links)
+
+### Description
+
+Built this site using HTML, CSS, js , jquery, and json. We had to create a day planner that saved and retained data in local storage after refresh.
+
+### Visuals
+
+![Screenshot of Site](assets\Work Day Schedul.png)
+
+### Usage
+
+This site shows a day planner. You can edit each time slot with text and hit save and it will store using local storage. You can leave the page and come back and your information will stay. You can overwrite the data as well. 
+
+### Links
+
+[Repo] (https://fatherwolf.github.io/Code-Quiz/)
+[Password-Generator] (https://github.com/FatherWolf/Code-Quiz)
--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,116 @@
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+
+
+// set item method 
+// give it a name and item 
+
+// localStorage.setItem("name", "austin engle"); 
+
+// localStorage.getItem("name ")
+
+// .val is pulling value of what user input
+// function getLocalStorage(){
+//     var v = localStorage.getItem(entryTime)
+//     console.log(v)
+// }
+
+
+
+
+    function save() {
+    $("#btn1").on("click", function (){
+        var entry = $("#time9").val();
+        
+        localStorage.setItem("9oclock", JSON.stringify(entry))
+
+    })}
+
+    function getLocalStorage(){
+        var v = JSON.parse(localStorage.getItem("9oclock"))
+        console.log(v)
+        if (v !== null) {
+            $("#time9").html(v);
+            } 
+    }
+    save()
+    getLocalStorage()
+
+
+    function save1() {
+        $("#btn2").on("click", function (){
+            var entry = $("#time10").val();
+            
+            localStorage.setItem("10oclock", JSON.stringify(entry))
+    
+        })}
+    
+        function getLocalStorage1(){
+            var v = JSON.parse(localStorage.getItem("10oclock"))
+            console.log(v)
+            if (v !== null) {
+                $("#time10").html(v);
+                } 
+        }
+        save1()
+        getLocalStorage1()
+
+    
+
+
+    // $("#btn2").on("click", function (){
+    //     var entry = $("#time10").val();
+    //     var entryTime = JSON.stringify(10)
+    //     localStorage.setItem(entryTime, entry)
+        
+    // })
+    
+
+    // $("#btn3").on("click", function (){
+    //     var entry = $("#time11").val();
+    //     var entryTime = JSON.stringify(11)
+    //     localStorage.setItem(entryTime, entry)
+    // })
+
+
+    // $("#btn4").on("click", function (){
+    //     var entry = $("#time12").val();
+    //     var entryTime = JSON.stringify(12)
+    //     localStorage.setItem(entryTime, entry)
+    // })
+
+
+    // $("#btn5").on("click", function (){
+    //     var entry = $("#time13").val();
+    //     var entryTime = JSON.stringify(1)
+    //     localStorage.setItem(entryTime, entry)
+    // })
+
+
+    // $("#btn6").on("click", function (){
+    //     var entry = $("#time14").val();
+    //     var entryTime = JSON.stringify(2)
+    //     localStorage.setItem(entryTime, entry)
+    // })
+
+
+    // $("#btn7").on("click", function (){
+    //     var entry = $("#time15").val();
+    //     var entryTime = JSON.stringify(3)
+    //     localStorage.setItem(entryTime, entry)
+    // })
+
+
+    // $("#btn8").on("click", function (){
+    //     var entry = $("#time16").val();
+    //     var entryTime = JSON.stringify(4)
+    //     localStorage.setItem(entryTime, entry)
+    // })
+
+
+    // $("#btn9").on("click", function (){
+    //     var entry = $("#time17").val();
+    //     var entryTime = JSON.stringify(5)
+    //     localStorage.setItem(entryTime, entry)
+    // })
+
